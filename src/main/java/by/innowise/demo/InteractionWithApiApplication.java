d086e26bbb27709d44ebebb40d06136b0f4e5974
Majorovaira <majorovaira8@gmail.com>
2020-12-21 03:33:56 +0300

    fix: optimize imports

--- src/main/java/by/innowise/demo/InteractionWithApiApplication.java
+++ src/main/java/by/innowise/demo/InteractionWithApiApplication.java
@@ -1,24 +1,10 @@
 package by.innowise.demo;
 
-import by.innowise.demo.controllers.DogController;
-import by.innowise.demo.entity.Dog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.io.IOException;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.temporal.TemporalUnit;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @EnableFeignClients
